95d44728bdb1abfb5adceca8e7e9618e1159e2d2
mr-shahjee <abdullahshahjee7@gmail.com>
2023-11-27 23:54:55 +0500

    part 1 crud implementation Clean

--- WebApi/Controllers/PatientController.cs
+++ WebApi/Controllers/PatientController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Patient.Queries;
+using Application.Features.Product.Command;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,13 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetPatients(CancellationToken cancellationToken)
         {
             var result = await _mediatR.Send(new GetAllPatientQuery(), cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpPost("CreatePatient")]
+        public async Task<IActionResult> CreatePatient(CreatePatientCommand createPatientCommand, CancellationToken cancellationToken)
+        {
+            var result = await _mediatR.Send(createPatientCommand, cancellationToken);
             return Ok(result);
         }
     }
